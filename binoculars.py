f2d7fa520f0c5473dde7eed31f5a0c31cc21a24a
Andrew Magill <andrew.b.magill@gmail.com>
2015-06-23 19:10:55 -0500

    still don't know what i'm looking at

--- binoculars.py
+++ binoculars.py
@@ -4,11 +4,15 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     CYAN = '\033[36m'
+    LIGHTBLUE = '\033[34m'
     BLUE = '\033[94m'
+    LIGHTGRAY = '\033[37m'
     DARKGRAY = '\033[90m'
     LIGHTRED = '\033[91m'
     YELLOW = '\033[93m'
     PURPLE = '\033[95m'
+    ONWHITE = '\033[48m'
+    WHITE = '\033[37m'
 
 def explode_bundle(file_path):
     """given the path to a bundle file, finds all
@@ -42,6 +46,66 @@ def explode_bundle(file_path):
         # close the map
         mm.close()
 
+def compare_bundlx(file_path1, file_path2):
+    file1 = open(file_path1, "r+b")
+    file2 = open(file_path2, "r+b")
+
+    mm1 = mmap.mmap(file1.fileno(), 0)
+    mm2 = mmap.mmap(file2.fileno(), 0)
+
+    for i in range(len(mm1)):
+
+        byte1 = mm1[i]
+        byte2 = mm2[i]
+
+        h1 = byte1.encode('hex')
+        h2 = byte2.encode('hex')
+
+        d1 = int(h1, 16)
+        d2 = int(h2, 16)
+
+        if d1 >= 32 and d1 <= 127:
+            char1 = byte1.encode('ascii').ljust(2)
+        else:
+            char1 = h1
+
+        if d2 >= 32 and d2 <= 127:
+            char2 = byte2.encode('ascii').ljust(2)
+        else:
+            char2 = h2
+
+
+        if mm1[i] == mm2[i]:
+            print bcolors.LIGHTBLUE + "%s" % char1,
+            print bcolors.LIGHTBLUE + "%s" % char2 + bcolors.ENDC,
+        else:
+            print bcolors.WHITE + "%s" % char1 + bcolors.ENDC,
+            print bcolors.WHITE + "%s" % char2 + bcolors.ENDC,
+
+        offset = i + 1
+
+        if offset % 16 == 0:
+            if not offset == 0:
+                print bcolors.DARKGRAY + "   %x " % i + bcolors.ENDC
+        elif offset % 4 == 0:
+            print bcolors.LIGHTGRAY + ':' + bcolors.ENDC,
+            #try:
+            #    m1 = int(str(mm2[i]), 16)
+            #    m2 = int(str(mm2[i]), 16)
+            #    m3 = int(str(mm2[i]), 16)
+            #    m4 = int(str(mm2[i]), 16)
+            #    print "%i %i %i %i" % (m1,m2,m3,m4),
+            #except:
+            #    pass
+        else:
+            print bcolors.DARKGRAY + ':' + bcolors.ENDC,
+
+        if (offset - 16) % 320 == 0:
+            for x in range(16):
+                print bcolors.DARKGRAY + "    %x  " % (x) + bcolors.ENDC,
+            #print
+            raw_input()
+
 def parse_bundlx(file_path):
     with open(file_path, "r+b") as f:
         # memory-map the file, size 0 means whole file
@@ -52,10 +116,13 @@ def parse_bundlx(file_path):
         line_size   = 16
         dword_size  = 4
         byte_count  = start_byte
+        line_no = 0
 
         for read_from in range(start_byte, len(mm), block_size):
 
             block = mm[read_from:read_from + block_size]
+
+            temp_list = []
 
             for read_from in range(0, len(block), line_size):
 
@@ -63,8 +130,8 @@ def parse_bundlx(file_path):
 
                 for read_from in range(0, len(line), dword_size):
 
-                    if not read_from == 0:
-                        print bcolors.DARKGRAY + "|" + bcolors.ENDC,
+                    #if not read_from == 0:
+                    #    print bcolors.DARKGRAY + "|" + bcolors.ENDC,
 
                     dword = line[read_from:read_from + dword_size]
 
@@ -79,37 +146,63 @@ def parse_bundlx(file_path):
                                 text_color = bcolors.YELLOW
                             else:
                                 text_color = bcolors.LIGHTRED
-
-                            print text_color + \
-                                  byte.encode('hex') + \
-                                  bcolors.DARKGRAY + '..' + \
-                                  bcolors.ENDC,
+                                temp_list.append((byte, line_no, block_pos, byte_count))
+                            pass
+                            #print text_color + \
+                            #      byte.encode('hex') + \
+                            #      bcolors.DARKGRAY + '..' + \
+                            #      bcolors.ENDC,
                         elif d >=  32 and d <= 127:
-                            print bcolors.PURPLE + " " + \
-                                  byte.encode('ascii') + \
-                                  bcolors.DARKGRAY + \
-                                  byte.encode('hex') + \
-                                  bcolors.ENDC,
+                            pass
+                            #print bcolors.PURPLE + " " + \
+                            #      byte.encode('ascii') + \
+                            #      bcolors.DARKGRAY + \
+                            #      byte.encode('hex') + \
+                            #      bcolors.ENDC,
                         else:
-                            print bcolors.CYAN + \
-                                  byte.encode('hex') + \
-                                  bcolors.DARKGRAY + '..' + \
-                                  bcolors.ENDC,
+                            pass
+                            #print bcolors.CYAN + \
+                            #      byte.encode('hex') + \
+                            #      bcolors.DARKGRAY + '..' + \
+                            #      bcolors.ENDC,
 
                         byte_count += 1
 
                 line_no = (byte_count / line_size)
-                print bcolors.DARKGRAY + "\t%x" % line_no + bcolors.ENDC
-            print
+                #print bcolors.DARKGRAY + "\t%x" % line_no + bcolors.ENDC,
+            #print
+
+            for item, line, position, count in temp_list:
+                if not item.encode('hex') == '00':
+                    try:
+                        char = bcolors.YELLOW + item.encode('ascii') + bcolors.DARKGRAY + " %x:%i:%x " % (line, position, count)
+                    except:
+                        char = bcolors.YELLOW + item.encode('hex') + bcolors.DARKGRAY + " %x:%i:%x " % (line, position, count)
+
+                    print char,
+            #print
 
         mm.close()
 
 def main():
     bundle_file_path = r"files/R0480C0380.bundle"
-    bundle_index_path = r"files/R0480C0380.bundlx"
+    #bundle_index_path1 = r"files/R0480C0380.bundlx"
+    #bundle_index_path2 = r"files/R251f80C1d4700.bundlx"
+
+    bundles = ["files/L00/R0480C0380.bundlx",
+               "files/L01/R0900C0700.bundlx",
+               "files/L02/R1280C0e80.bundlx",
+               "files/L03/R2500C1d00.bundlx",
+              ["files/L04/R4a00C3a00.bundlx",
+               "files/L04/R4a00C3a80.bundlx"],
+              ["files/L05/R9400C7480.bundlx",
+               "files/L05/R9400C7500.bundlx",
+               "files/L05/R9480C7480.bundlx",
+               "files/L05/R9480C7500.bundlx"]]
 
     #explode_bundle(bundle_file_path)
-    parse_bundlx(bundle_index_path)
+    #parse_bundlx(bundle_index_path)
+    compare_bundlx(bundles[0], bundles[1])
 
 if __name__== "__main__":
     main()
